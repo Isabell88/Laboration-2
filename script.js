708066d874d5523a516b9b4e8771082f4c376399
Isabell Betar <isabellbetar@Isabells-MacBook-Pro.local>
2020-11-10 21:50:34 +0100

    add js content and styling

---
+++ script.js
@@ -0,0 +1,24 @@
+const mobilMenu = document.querySelector(".mobilMenu");
+const dropdownBars = document.getElementById("dropdown");
+
+dropdownBars.onclick = function() {
+    if (mobilMenu.style.height == "50%") {
+        mobilMenu.style.height = null;
+    } else {
+        mobilMenu.style.height = "50%";
+    }
+}
+
+var nameText = "ISABELL BETAR";
+var nameArray = nameText.split("");
+var loopTimer;
+function frameLooper( ) {
+    if(nameArray.length > 0) {
+document.getElementById("type-name").innerHTML += nameArray.shift();
+    } else {
+        clearTimeout(loopTimer);
+        return false;
+    }
+    loopTimer = setTimeout("frameLooper()",100);
+}
+frameLooper();
